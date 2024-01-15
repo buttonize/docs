94f5c6fb57e90f6fb4858209596d2da821c3c635
Filip Pýrek <PyrekFilip@gmail.com>
2024-01-16 00:21:49 +0100

    feat: refactor linting/formatting, create Astro components

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -2,5 +2,15 @@ module.exports = {
 	semi: false,
 	singleQuote: true,
 	useTabs: true,
-	trailingComma: 'none'
+	trailingComma: 'none',
+	plugins: ['prettier-plugin-astro', 'prettier-plugin-astro'],
+	tailwindConfig: './docs/tailwind.config.js',
+	overrides: [
+		{
+			files: '*.astro',
+			options: {
+				parser: 'astro'
+			}
+		}
+	]
 }
